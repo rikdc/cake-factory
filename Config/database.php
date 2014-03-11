2a000776f994e3a84d7ce9ffc444360178c8ad3a
Jaggy Gauran <jaggygauran@gmail.com>
2014-03-11 23:43:27 +0800

    made the database connection work.

--- Config/database.php
+++ Config/database.php
@@ -6,7 +6,7 @@ class DATABASE_CONFIG
   public $test = array(
     'datasource' => 'Database/Sqlite',
     'persistent' => false,
-    'database'   => 'factory',
+    'database'   => 'Config/factory.db',
     'prefix'     => '',
     'encoding'   => 'utf8'
   );
