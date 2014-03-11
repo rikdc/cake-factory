c0ad1b7adc5d68346696845e9abacdb6ec3dd353
Jaggy Gauran <jaggygauran@gmail.com>
2014-03-11 20:03:53 +0800

    initialize a model

--- Lib/Factory.php
+++ Lib/Factory.php
@@ -4,4 +4,51 @@ App::uses( 'File', 'Utility' );
 class Factory extends Object
 {
 
+  /**
+   * the name of the model to be initialized
+   *
+   * @var integer
+   */
+  protected $name;
+
+
+  /**
+   * the instance for the factories to work
+   *
+   * @var Model
+   */
+  protected $model;
+
+  /**
+   * Initialize everything!
+   *
+   * @param string $name
+   */
+  public function __construct( $name )
+  {
+    $this->name  = ucfirst( $name );
+    $this->model = ClassRegistry::init( $name );
+  }
+
+
+  /**
+   * Return the model name to be used
+   *
+   * @return string
+   */
+  function getName()
+  {
+    return $this->name;
+  }
+
+  /**
+   * return the model
+   *
+   * @return Model
+   */
+  function getModel()
+  {
+    return $this->model;
+  }
+
 }
--- spec/FactorySpec.php
+++ spec/FactorySpec.php
@@ -5,10 +5,31 @@ namespace spec;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+define( 'ROOT', dirname(dirname(__FILE__)) );
+define( 'DS', DIRECTORY_SEPARATOR );
+include_once ROOT . DS . 'vendor' . DS . 'pear-pear.cakephp.org' . DS . 'CakePHP' . DS . 'Cake' . DS . 'basics.php';
+
 class FactorySpec extends ObjectBehavior
 {
-    function it_is_initializable()
-    {
-        $this->shouldHaveType('Factory');
-    }
+
+  function let()
+  {
+    $this->beConstructedWith( 'user' );
+  }
+
+  function it_is_initializable()
+  {
+    $this->shouldHaveType('Factory');
+  }
+
+  function it_can_return_the_model_name_to_be_parsed()
+  {
+    $this->getName()->shouldBe( 'User' );
+  }
+
+  function it_initializes_a_model()
+  {
+    $this->getModel()->shouldReturnAnInstanceOf( 'Model' );
+  }
+
 }
