95acab8e5cfbd1b5606ed4697cd884f602ef4183
Jaggy Gauran <jaggygauran@gmail.com>
2014-03-12 11:57:36 +0800

    made the associations working.

--- spec/bootstrap.php
+++ spec/bootstrap.php
@@ -21,3 +21,31 @@ define( 'CAKE', APP . 'vendor' . DS . 'pear-pear.cakephp.org' . DS . 'CakePHP' .
 
 // load the CakePHP global methods to not fuck things up
 require_once CAKE . DS . 'basics.php';
+
+
+/**
+ * Create the test database
+ *
+ */
+function createDatabase()
+{
+  $config   = APP . 'Config';
+  $database = $config . DS . 'factory.db';
+  $schema   = $config . DS . 'schema.sql';
+
+  $command = "sqlite3 {$database} < {$schema}";
+  system( $command );
+}
+
+
+/**
+ * Delete the database
+ *
+ */
+function deleteDatabase()
+{
+  $database = APP . 'Config' . DS .'factory.db';
+  $command  = "rm {$database}";
+
+  system( $command );
+}
